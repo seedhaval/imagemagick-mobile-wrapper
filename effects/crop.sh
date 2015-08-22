aae1a922a289a1a7846f19a850783cc785d58908
seedhaval <seedhaval@gmail.com>
2015-08-22 17:32:09 +0530

    first commit

--- effects/crop.sh
+++ effects/crop.sh
@@ -3,4 +3,4 @@ height=$2
 offsetx=$3
 offsety=$4
 
-convert tmp.gif -crop "$width"x"$height"+"$offsetx"+"$offsety" tmp.gif
+convert tmp.gif -crop "$width"x"$height"+"$offsetx"+"$offsety" +repage tmp.gif
